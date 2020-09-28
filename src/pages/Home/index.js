bc78c8e757cbc114b4016312920cff39a92827e5
Jeziel Marques <jezielmarques7@gmail.com>
2020-09-27 23:30:07 -0300

    initial ci

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+import { Container, Title } from './styles';
+
+const Home = () => {
+  return (
+    <Container>
+      <Title>Async Storage - RN</Title>
+    </Container>
+  );
+};
+
+export default Home;
