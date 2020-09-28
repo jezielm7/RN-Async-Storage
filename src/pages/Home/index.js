0818f54f82379d554921400b86765327e9b8545b
Jeziel Marques <jezielmarques7@gmail.com>
2020-09-28 04:18:54 -0300

    finalized functions

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,85 @@
 import React, { useState } from 'react';
+import { Alert } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  Input,
+  ButtonContainer,
+  Button,
+  BtnText,
+  DataButton,
+  DataBtnText,
+  DelButton,
+  DelBtnText,
+} from './styles';
 
 const Home = () => {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+
+  async function saveData() {
+    const user = {
+      name,
+      age,
+    };
+
+    await AsyncStorage.setItem('user', JSON.stringify(user));
+  }
+
+  async function showUser() {
+    const userJson = await AsyncStorage.getItem('user');
+    const user = JSON.parse(userJson);
+
+    if (!user) {
+      Alert.alert(
+        'User Infos',
+        `Don't find user's info!`
+      );
+    }
+
+    Alert.alert(
+      'User Infos',
+      `Name: ${user.name} - Age: ${user.age}`
+    );
+  }
+
+  async function deleteUser() {
+    await AsyncStorage.removeItem('user')
+  }
+
   return (
     <Container>
       <Title>Async Storage - RN</Title>
+
+      <Input
+        value={name}
+        placeholder='Type your name'
+        placeholderTextColor='#eee'
+        onChangeText={text => setName(text)}
+      />
+
+      <Input
+        value={age}
+        placeholder='Type your age'
+        placeholderTextColor='#eee'
+        onChangeText={text => setAge(text)}
+      />
+
+      <Button onPress={saveData}>
+        <BtnText>Save</BtnText>
+      </Button>
+
+      <ButtonContainer>
+        <DataButton onPress={showUser}>
+          <DataBtnText>💾</DataBtnText>
+        </DataButton>
+
+        <DelButton onPress={deleteUser}>
+          <DelBtnText>🗑</DelBtnText>
+        </DelButton>
+      </ButtonContainer>
     </Container>
   );
 };
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -7,7 +7,59 @@ export const Container = styled.View`
 `;
 
 export const Title = styled.Text`
-  font-size: 15px;
+  margin: 10px 0;
+  font-size: 25px;
   align-items: center;
   justify-content: center;
+`;
+
+export const Input = styled.TextInput`
+  color: #eee;
+  margin: 10px 0;
+  font-size: 16px;
+  padding: 10px 50px;
+  border-radius: 10px;
+  background: #607D8B;
+`;
+
+export const ButtonContainer = styled.View`
+  margin-top: 15px;
+  flex-direction: row;
+`;
+
+export const Button = styled.TouchableOpacity`
+  margin-top: 20px;
+  padding: 15px 80px;
+  border-radius: 10px;
+  background: #607D8B;
+`;
+
+export const BtnText = styled.Text`
+  color: #eee;
+  font-size: 16.5px;
+`;
+
+export const DataButton = styled.TouchableOpacity`
+  margin-top: 10px;
+  margin-right: 50px;
+  padding: 15px 15px;
+  border-radius: 10px;
+  background: #009688;
+`;
+
+export const DataBtnText = styled.Text`
+  color: #eee;
+  font-size: 16.5px;
+`;
+
+export const DelButton = styled.TouchableOpacity`
+  margin-top: 10px;
+  padding: 15px 15px;
+  border-radius: 10px;
+  background: #E53935;
+`;
+
+export const DelBtnText = styled.Text`
+  color: #eee;
+  font-size: 16.5px;
 `;
